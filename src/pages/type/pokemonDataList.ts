de8db0890f1a52ac96246c561f10e7ad884513de
watanabe0055 <mailok1212+github@gmail.com>
2023-03-12 08:28:08 +0900

    feat: ポケモンAPIをフェッチ

---
+++ src/pages/type/pokemonDataList.ts
@@ -0,0 +1,21 @@
+/**
+ * @type ポケモン一覧APIのレスポンス
+ */
+export type PokemonDataList = {
+  count: number
+  next?: string
+  previous?: string
+  Results: PokemonResults
+}
+
+/**
+ * @type ポケモン一覧APIのリザルトの中身
+ */
+type PokemonResults = {
+  name: string
+  url: string
+}
+
+export type ResponsePokemonDataList = {
+  data: PokemonDataList
+}
