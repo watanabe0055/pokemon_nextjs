b62b45d289d95d5d1960fe7a5132d8dfc9700826
watanabe0055 <mailok1212+github@gmail.com>
2023-03-12 23:19:15 +0900

    fix: 不要なimpoertwo

--- src/atom/pokemonName/index.tsx
+++ src/atom/pokemonName/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import styles from '@/atom/pokemonName/index.module.scss'
-//import { PokemonNameProps } from '@/type/PokemonCard'
 
 export const PokemonName = (props) => {
   const { name } = props
