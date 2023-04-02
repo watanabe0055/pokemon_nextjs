99eea34bbbc8ba3f0a3bc46b4049e0fe2df8cd9b
watanabe0055 <mailok1212+github@gmail.com>
2023-04-03 01:09:34 +0900

    feat: タイプ一覧

---
+++ src/constant/pokeonType.ts
@@ -0,0 +1,22 @@
+/** タイプの一覧 */
+// TODO: 検討して修正
+export const POKEMON_TYPE = {
+  NORMAL: 'ノーマル',
+  FIGHTING: 'かくとう',
+  FLYING: 'ひこう',
+  POISON: 'どく',
+  GROUND: 'じめん',
+  ROCK: 'いわ',
+  BUG: 'むし',
+  GHOST: 'ゴースト',
+  STEEL: 'はがね',
+  FIRE: 'ほのお',
+  WATER: 'みず',
+  GRASS: 'くさ',
+  ELECTRIC: 'でんき',
+  PSYCHIC: 'エスパー',
+  ICE: 'こおり',
+  DRAGON: 'ドラゴン',
+  DARK: 'あく',
+  FAIRY: 'フェアリー',
+}
